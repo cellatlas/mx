75492d4e78777512b11eeb2355610f20ea0b8cf8
Sina Booeshaghi <sbooeshaghi@gmail.com>
2022-11-15 18:10:46 -0800

    fixed reqs for installation

--- setup.py
+++ setup.py
@@ -2,43 +2,41 @@ from setuptools import find_packages, setup
 
 
 def read(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read()
 
 
-long_description = read('README.md')
+long_description = read("README.md")
 
 setup(
-    name='mx',
-    version='0.0.0',
-    url='https://github.com/sbooeshaghi/mx',
-    author='Sina Booeshaghi',
-    author_email='alisina@caltech.edu',
-    maintainer='Sina Booeshaghi',
-    maintainer_email='alisina@caltech.edu',
-    description='',  # noqa
+    name="mx",
+    version="0.0.0",
+    url="https://github.com/sbooeshaghi/mx",
+    author="Sina Booeshaghi",
+    author_email="alisina@caltech.edu",
+    maintainer="Sina Booeshaghi",
+    maintainer_email="alisina@caltech.edu",
+    description="",  # noqa
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='',
-    python_requires='>=3.6',
-    license='MIT',
-    packages=find_packages(exclude=('tests', 'tests.*')),
+    long_description_content_type="text/markdown",
+    keywords="",
+    python_requires=">=3.8",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "tests.*")),
     zip_safe=False,
     include_package_data=True,
-    install_requires=read('requirements.txt').strip().split('\n'),
+    install_requires=read("requirements.txt").strip().split("\n"),
     entry_points={
-        'console_scripts': ['mx=mx.main:main'],
+        "console_scripts": ["mx=mx.main:main"],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Utilities",
     ],
 )
